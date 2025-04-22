6af9585b1c7e1fcf7a63f2ffcd09de5bf7091630
tali13 <talitanaibert.c@gmail.com>
2025-04-22 15:35:20 -0300

    database

---
+++ taskManager/src/main/java/com/mycompany/taskmanager/database/ConnectionSQL.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taskmanager.database;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author TALITANAIBERTDACOSTA
+ */
+public class ConnectionSQL {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static Connection connection;
+
+ 
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String DB_USER = dotenv.get("DB_USER");
+    private static final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+
+  
+    public static Connection conectar() {
+        try {
+        
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, DB_USER, DB_PASSWORD);
+            
+                System.out.println("Conectado ao banco.");
+            }
+        } catch (SQLException error) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", error);
+        }
+
+        return connection;
+    }
+}
