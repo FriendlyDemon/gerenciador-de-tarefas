a5a64fcf763d1ea5bedc06c68349a9b2c86c57a3
friendlydemon <henriquemichel@live.com>
2025-04-22 14:28:52 -0300

    inicio

---
+++ taskManager/src/main/java/com/mycompany/taskmanager/TaskManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taskmanager;
+
+/**
+ *
+ * @author HENRIQUEMICHEL
+ */
+public class TaskManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
